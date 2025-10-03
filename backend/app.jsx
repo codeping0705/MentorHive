418e33898595b35c45103c4bed0f7072f9fdbcff
Sai <saiprithvi0705@gmail.com>
2025-10-03 19:24:39 +0530

    get function2

--- backend/app.jsx
+++ backend/app.jsx
@@ -13,6 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  });
+});
+
 app.use(cookieParser());
 
 app.use(config.PREFIX, routes);
