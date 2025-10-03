61ef41b0da32b5fce3a7422c9febb3e2ef9fe441
Sai <saiprithvi0705@gmail.com>
2025-10-03 19:25:25 +0530

    get function2

--- backend/app.jsx
+++ backend/app.jsx
@@ -7,20 +7,13 @@ require("./config/db.js");
 
 const routes = require("./routes/v1");
 const config = require("./config");
-const { error } = require("console");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send({
-    activeStatus: true,
-    error: false,
-  });
-});
 
-app.use(cookieParser());
+app.use(cookieParser);
 
 app.use(config.PREFIX, routes);
 app.use();
