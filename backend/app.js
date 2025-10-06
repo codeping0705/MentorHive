3b0dfefd8043e87eeb423dff748a546a97989e1b
Sai <saiprithvi0705@gmail.com>
2025-10-06 13:39:34 +0530

    fixed mongodb again!

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Health check
-app.get("/v1", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ activeStatus: true, error: false });
 });
 
