ceecbe4843c80a03c7db242fc6e4c63deffeb503
Sai <saiprithvi0705@gmail.com>
2025-10-06 13:45:36 +0530

    routes folder fixed!

--- backend/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-// routes/v1.js
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ message: "API v1 working!" });
-});
-
-module.exports = router;
